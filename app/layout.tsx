3cb70d8201def7c6b282cf1d9dc522b2cf302974
lukeschaller <lukedschaller@gmail.com>
2025-06-14 20:50:44 -0500

    Set up Google tag.

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,18 +1,37 @@
 import type { Metadata } from 'next'
+import { Inter } from 'next/font/google'
 import './globals.css'
+import Script from 'next/script'
+
+const inter = Inter({ subsets: ['latin'] })
 
 export const metadata: Metadata = {
-  title: 'Luke Schaller',
+  title: 'Luke Schaller - Software Engineer',
+  description: 'Personal Portfolio of Luke Schaller, Software Engineer and Technology Intern',
 }
 
 export default function RootLayout({
   children,
-}: Readonly<{
+}: {
   children: React.ReactNode
-}>) {
+}) {
   return (
     <html lang="en">
-      <body>{children}</body>
+      <head>
+        <Script
+          src="https://www.googletagmanager.com/gtag/js?id=G-R73ZYZ8SZ0"
+          strategy="afterInteractive"
+        />
+        <Script id="google-analytics" strategy="afterInteractive">
+          {`
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+            gtag('config', 'G-R73ZYZ8SZ0');
+          `}
+        </Script>
+      </head>
+      <body className={inter.className}>{children}</body>
     </html>
   )
 }
